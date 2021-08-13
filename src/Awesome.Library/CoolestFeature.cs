86b20bb32c819670f33b9906c1da55bad8a540c0
이준환 <joonhwan.lee@mirero.co.kr>
2021-08-13 09:17:59 +0900

    part2. 완성

---
+++ src/Awesome.Library/CoolestFeature.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Awesome.Library
+{
+    /// <summary>
+    /// Hello this is **Class1** from *Awesome.Library*
+    /// </summary>
+    public class CoolestFeature
+    {
+        private InnerClass _class;
+        public int Value { get; }
+
+        /// <summary>
+        /// This is a ctor
+        /// </summary>
+        /// <param name="value">The value of the class</param>
+        public CoolestFeature(int value)
+        {
+            Value = value;
+        }
+
+        public double ConvertToDouble()
+        {
+            return Value;
+        }
+
+        /// <summary>
+        /// A method referencing a inner class
+        /// </summary>
+        /// <param name="name">The name</param>
+        /// <param name="inner">A inner class with type <seealso cref="InnerClass"/></param>
+        public void SetInnerClass(string name, InnerClass inner)
+        {
+            inner.Name = name;
+            _class = inner;
+        }
+
+        public class InnerClass
+        {
+            public string Name { get; set; }
+        }
+    }
+}
